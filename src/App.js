3e1ccec4205d56fe65dab47a35e10c4edf974211
chentianran <chentianran@riz-tech.com>
2023-06-04 15:39:22 +0800

    列表渲染

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
-
+//1.识别常规的变量
+//2.原生js方法调用
+const otto='电棍'
+const getName = ()=>{
+  return '欧内的手好汉'
+}
+const flag = 0
+const songs = [
+  {id:1,name:'欧内的手'},
+  {id:2,name:'奥丽安费'},
+  {id:3,name:'尊尼获加'},
+]
 function App() {
   return (
     <div className="App">
-      app
+      { otto }
+      <br></br>
+      { getName() }
+      <br></br>
+      { flag ? '可以的':'铸币吧'}
+      <br></br>
+      <ul>
+        {songs.map(song=> <li key={song.id}>{song.name}</li>)}
+      </ul>
     </div>
   );
 }
