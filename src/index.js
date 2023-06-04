a490aef238a632771c8ecd66194704428f04b3a3
chentianran <chentianran@riz-tech.com>
2023-06-04 14:55:26 +0800

    react项目的创建

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//React 核心包
+import React from 'react';
+//React 渲染包
+import ReactDOM from 'react-dom/client';
+//全局样式
+import './index.css';
+//入口文件
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  //严格模式节点需要去掉
+  //userEffect的执行时机
     <App />
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
